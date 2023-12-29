6f900b81d692acef78f8e602e321e49d004b0bbe
Alex Conway <abconway2@gmail.com>
2023-12-29 12:38:44 -0500

    Add simple enetmultiplayer client/server connection

--- nodes/experiments/multiplayer/MultiplayerExperiment.gd
+++ nodes/experiments/multiplayer/MultiplayerExperiment.gd
@@ -1,2 +1,11 @@
 extends Node2D
 class_name MultiplayerExperiment
+
+
+
+func _on_client_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://nodes/experiments/multiplayer/Client.tscn")
+
+
+func _on_server_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://nodes/experiments/multiplayer/Server.tscn")
--- nodes/experiments/multiplayer/Server.gd
+++ nodes/experiments/multiplayer/Server.gd
@@ -1,14 +1,19 @@
 extends Node
 
+
 const IP_ADDRESS = '127.0.0.1'
 const PORT = 27572
 const MAX_CLIENTS = 1
 
+var peer_server := ENetMultiplayerPeer.new()
+
 
 func _ready() -> void:	
-	# Create Server
-	var peer_server = ENetMultiplayerPeer.new()
 	peer_server.create_server(PORT, MAX_CLIENTS)
 	multiplayer.multiplayer_peer = peer_server
+	multiplayer.peer_connected.connect(_on_peer_connected)
 	print(multiplayer.is_server())
-	
+
+
+func _on_peer_connected(peer_id: int) -> void:
+	print("Peer Connected: ", peer_id)
